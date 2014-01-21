06d9f3718d8cec52d8e638750705498fb2409bed
Ryan Morey <ryantmorey@gmail.com>
2014-01-21 01:46:47 -0500

    Cleaned stuff, added better doc

--- robot.h
+++ robot.h
@@ -1,58 +1,74 @@
+/*
+A few conventions:
+BTN/btn denotes a controller button, returns 1 if pressed, 0 otherwise (actually maybe they return booleans...)
+JS/js denotes a joystick coordinate, an int in (-127,127), remember that pushing up on the js returns a negative y-coord
+ENC/enc denotes an encoder value, an int, 1440 ticks to a rotation (dont ask me why...)
+PWR/pwr denotes a motor power as an int in (-100,100)
 
-//driver
-#define FLAG_BTN joy1Btn(1)
-#define TILT_BTN joy1Btn(2)
-#define BELT_IN_BTN joy1Btn(5)
-#define BELT_OUT_BTN joy1Btn(6)
+ALL CAPS means a constant
+lowerCase means a variable
+*/
 
-//scorer
-#define SCORE_BTN joy2Btn(1)
-#define KILL_BTN (joy1Btn(10) || joy2Btn(10))
+//driver controls 
+#define BTN_FLAG joy1Btn(1) //hold to spin flag	
+#define BTN_TILT joy1Btn(2) //toggles drive mode
+#define BTN_BELT_IN joy1Btn(5) //hold to pull blocks in
+#define BTN_BELT_OUT joy1Btn(6) //hold to push blocks out 
+#define JS_DRIVE_X joystick.joy1_x1 
+#define JS_DRIVE_Y joystick.joy1_y1
+#define JS_ROTATE joystick.joy1_x2
 
-//joysticks
-#define DRIVE_JS_X joystick.joy1_x1
-#define DRIVE_JS_Y joystick.joy1_y1
-#define ROTATE_JS joystick.joy1_x2
-#define BUCKET_JS joystick.joy2_y1
+//scorer controls
+#define BTN_SCORE joy2Btn(1) //scores blocks
+#define BTN_ENC_RESET joy2Btn(2) //reset the bucket encoder values to zero
+#define JS_BUCKET joystick.joy2_y1
 
-void goForward (int power) {// these functions go in the specified direction at the specified power
-	motor[M_drive_FL] = power;
-	motor[M_drive_FR] = power;
-	motor[M_drive_BR] = power;
-	motor[M_drive_BL] = power;
+//both drivers
+#define BTN_KILL (joy1Btn(10) || joy2Btn(10))
+
+//encoder defs
+#define ENC_BUCKET_MAX 9001 //TODO: figure this out
+
+//go in the specified direction at the specified power
+void goForward (int pwr) {
+	motor[M_drive_FL] = pwr;
+	motor[M_drive_FR] = pwr;
+	motor[M_drive_BR] = pwr;
+	motor[M_drive_BL] = pwr;
 }
-void goBack (int power) {
-	goForward(-power);
+void goBack (int pwr) {
+	goForward(-pwr);
 }
-void goRight (int power) {
-	motor[M_drive_FL] = power;
-	motor[M_drive_FR] = power;
-	motor[M_drive_BR] = -power;
-	motor[M_drive_BL] = -power;
+void goRight (int pwr) {
+	motor[M_drive_FL] = pwr;
+	motor[M_drive_FR] = pwr;
+	motor[M_drive_BR] = -pwr;
+	motor[M_drive_BL] = -pwr;
 }
-void goLeft (int power) {
-	goRight(-power);
+void goLeft (int pwr) {
+	goRight(-pwr);
 }
-void goForwardRight (int power) {
-	motor[M_drive_FL] = power;
+void goForwardRight (int pwr) {
+	motor[M_drive_FL] = pwr;
 	motor[M_drive_FR] = 0;
-	motor[M_drive_BR] = power;
+	motor[M_drive_BR] = pwr;
 	motor[M_drive_BL] = 0;
 }
-void goBackLeft (int power) {
-	goForwardRight(-power);
+void goBackLeft (int pwr) {
+	goForwardRight(-pwr);
 }
-void goForwardLeft (int power) {
+void goForwardLeft (int pwr) {
 	motor[M_drive_FL] = 0;
-	motor[M_drive_FR] = power;
+	motor[M_drive_FR] = pwr;
 	motor[M_drive_BR] = 0;
-	motor[M_drive_BL] = power;
+	motor[M_drive_BL] = pwr;
 }
-void goBackRight (int power) {
-	goForwardLeft(-power);
+void goBackRight (int pwr) {
+	goForwardLeft(-pwr);
 }
 
-void haltAllMotors () { //stops all motors
+//stops all motors
+void haltAllMotors () { 
 	motor[M_drive_FL] = 0;
 	motor[M_drive_FR] = 0;
 	motor[M_drive_BR] = 0;
@@ -63,6 +79,7 @@ void haltAllMotors () { //stops all motors
 	motor[M_belt] = 0;
 }
 
+//raise and lower bucket using time, kinda sketch..
 void raiseBucket() {
 	motor[M_slider_L] = 25;
 	motor[M_slider_R] = 25;
@@ -79,7 +96,8 @@ void lowerBucket() {
 	motor[M_slider_R] = 0;
 }
 
-void scoreBlocks() { //pushes blocks out and brings pusher back in
+//pushes blocks out and brings pusher back in
+void scoreBlocks() {
 	servo[S_score] = 0;
 	wait1Msec(1400);
 	servo[S_score] = 255;
